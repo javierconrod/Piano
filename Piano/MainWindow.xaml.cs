4011f6c8d7104ae21eb1fd0f3824237f02f7578f
KarenLopezB <Karen_lopez1936@hotmail.com>
2020-03-12 12:47:39 -0700

    Nota Do

--- Piano/MainWindow.xaml.cs
+++ Piano/MainWindow.xaml.cs
@@ -13,6 +13,10 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using NAudio.Mixer;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+
 namespace Piano
 {
     /// <summary>
@@ -20,9 +24,32 @@ namespace Piano
     /// </summary>
     public partial class MainWindow : Window
     {
+        private WaveOut waveOut;
+
+        private MixingSampleProvider mixer;
+
         public MainWindow()
         {
+
             InitializeComponent();
+            waveOut = new WaveOut();
+            mixer = new MixingSampleProvider(WaveFormat.CreateIeeeFloatWaveFormat(44100, 1));
+            mixer.ReadFully = true;
+            waveOut.Init(mixer);
+            waveOut.Play();
+
+        }
+
+        private void BtnC_Click(object sender, RoutedEventArgs e)
+        {
+            var nota_c = new SignalGenerator(44100, 1)
+            {
+                Gain = 0.5f,
+                Frequency = 261.6,
+                Type = SignalGeneratorType.Sin
+            }.Take(TimeSpan.FromMilliseconds(250));
+
+            mixer.AddMixerInput(nota_c);
         }
     }
 }
